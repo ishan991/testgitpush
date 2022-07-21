ab7ee56aa7ff837fcbac275a6b928e51e2984cf7
ishan <ishandwivedi100@Gmail.com>
2022-07-21 21:42:18 +0530

    this is my first commit and push in git.

---
+++ mongotest1.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://ishan991:ishan@ishan.7u6ck.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)      # if it print without error it means it has correct connection
+
+d= {
+    "name" : "ishan",
+    "email" : "ishandwivedi100@gmail.com",
+    "surname" : "dwivedi"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
